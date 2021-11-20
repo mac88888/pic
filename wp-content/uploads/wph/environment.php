d80ead3ec3cadc81aecc844374dea41820409739
macoscrack8 <93522079+macoscrack8@users.noreply.github.com>
2021-11-20 15:53:26 +0800

    Add files via upload

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,3 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$environment_variable = '{"themes":{"1":{"themes_url":"https:\/\/macoscrack.com\/wp-content\/themes","use_child_theme":false,"main":{"folder_name":"Total","mapped_name":""}}},"allowed_paths":["\/home\/customer\/www\/macoscrack.com\/public_html\/wp-content\/themes"],"wordpress_directory":"","site_relative_path":"\/"}'; ?>
