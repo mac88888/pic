a94d8b1579e43a882b317f8a382006b52192f4e6
macoscrack8 <93522079+macoscrack8@users.noreply.github.com>
2021-12-17 11:27:18 +0800

    Add files via upload

---
+++ wp-content/plugins/prisna-wp-translate/javascript/blocks.class.js
@@ -0,0 +1,14 @@
+/**
+ * Prisna WP Translate
+ * https://www.prisna.net/
+ *
+ * LICENSE
+ *
+ * @package    Prisna WP Translate
+ * @copyright  Copyright (c) 2021, Prisna Ltd, www.prisna.net
+ * @license    https://www.prisna.net/license/
+ * @version    1.15.20
+ * @date       2021-10-30
+ */
+
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(8(a){a.7.X("3-6-2/c",{q:"C N",D:"R k G K",U:"w",T:"3-2",S:["w","2","Q"],P:{O:!1,V:!1,M:!1,L:!1},h:{n:{i:"J","I":"o"}},H:{A:[{i:"j",F:8(b){d"[3-6-2]"===b.z},7:["g/f"],s:8(){d a.7.v("3-6-2/c",{n:"o"})}}],B:[{i:"j",7:["g/f"],s:8(){d a.7.v("g/f",{z:"[3-6-2]"})}}]},E:8(b){d[a.4.5(a.m.u,9,a.4.5("p",{1b:{1f:"0 1g"}},"1h 1i 1j 1k a 1l W 1n 1o k c, 1p 1q, 1r 1e 1d Y 1c.")),a.4.5(a.m.u,9,a.4.5(a.t.1a,{q:"19",18:!1},9,a.4.5(a.t.17,9,a.4.5("a",{e:x.y.e.16(0,x.y.e.15("/"))+"/14.13?l=3-2-r",12:"11"},"10 k r l")))),a.4.5(a.Z,{j:"3-6-2/c",h:b.h})]}})})(1m.6);',62,90,'||translate|prisna|element|createElement|wp|blocks|function|null|||translator|return|href|shortcode|core|attributes|type|block|the|page|blockEditor|is_preview|false||title|settings|transform|components|InspectorControls|createBlock|translation|self|location|text|from|to|Prisna|description|edit|isMatch|language|transforms|default|string|switcher|reusable|html|Translate|customClassName|supports|languages|Insert|keywords|category|icon|className|visual|registerBlockType|translations|serverSideRender|Open|_blank|target|php|plugins|lastIndexOf|substring|PanelRow|initialOpen|Settings|PanelBody|style|here|perform|cannot|padding|10px|Note|that|this|is|just|window|representation|of|and|thus|it'.split('|'),0,{}))
